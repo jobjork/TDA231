7411a39c99b1d0977c34f2a988797fd68ed7eeb4
Johan Björk <jobjork@hotmail.com>
2017-01-27 15:19:12 +0100

    Started coding  assignment 2.1

---
+++ PlotSample.m
@@ -0,0 +1,46 @@
+function [ samplePlot ] = PlotSample(x)
+hold on;
+
+[mu, sigma] = sge(x);
+numberOfCircles = 3;
+numSamples = length(x);
+
+scatter(x(:,1), x(:,2), '.');
+
+pointsOutsideCircle = zeros(numSamples, numberOfCircles);
+
+xCenter = mu(1);
+yCenter = mu(2);
+
+angle = linspace(0, 2*pi, 1000);
+
+for k=1:numberOfCircles
+    
+    for i=1:numSamples
+        outside = (x(i, 1) - xCenter)^2 + (x(i,2) - yCenter)^2 > k*sigma;
+        if outside
+            pointsOutsideCircle(i, k) = 1;
+        end
+        
+    end
+    
+    xCoord = k*sigma * cos(angle) + xCenter;
+    yCoord = k*sigma * sin(angle) + yCenter;
+   
+    fractionOutside = sum(pointsOutsideCircle(:,k))/numSamples;
+    
+    set(gca,'fontsize',14)
+    
+
+    plot(xCoord, yCoord, 'DisplayName', strcat('cos(x)' , num2str(k)))
+
+    legend('show');
+    
+    
+end
+
+%samplePlot;
+
+
+end
+
---
+++ sge.m
@@ -0,0 +1,21 @@
+function [mu, sigma] = sge(x)
+%
+% SGE Mean and variance estimator for spherical Gaussian distribution                               
+%
+% x     : Data matrix of size n x p where each row represents a 
+%         p-dimensional data point e.g. 
+%            x = [2 1;
+%                 3 7;
+%                 4 5 ] is a dataset having 3 samples each
+%                 having two co-ordinates.
+%
+% mu    : Estimated mean of the dataset [mu_1 mu_2 ... mu_p] 
+% sigma : Estimated standard deviation of the dataset (number)                 
+%
+
+dimension = length(x(1,:));
+numMeasurements = length(x);
+
+mu = mean(x);
+sigma = sqrt(sum(sum((x-repmat(mu, 1000, 1)).^2))/(dimension*numMeasurements));
+
