9d0fecd0afe593e1f1c6f9cde000a8649acd1262
OlofEkborg <olofekborg@gmail.com>
2017-02-01 11:52:47 +0100

    progress in CV on sph_bayes

--- HW2/HW2_2_2.m
+++ HW2/HW2_2_2.m
@@ -11,10 +11,22 @@ for k=1:kFold
     samples(:,k) = sampleIndices(((k-1)*partitionLength +1):k*partitionLength);
 end
 
+%%
+tot_error=zeros(1,kFold);
 
 for k=1:kFold
-    test=x(samples(:,k),:);
-    train=x(-samples(:,k),:);
+    testX=x(samples(:,k),:);
+    samp_train=samples;
+    samp_train(:,k)=[];
+    trainX=x(samp_train,:);
+    trainY=y(samp_train,:);
 
+    error=zeros(1,length(testX));
+    for n=1:length(testX)
+        [P1,P2,Y]=sph_bayes(testX(n,:),trainX,trainY);
+        error(n)=Y-y(samples(n,k));
+    end
+    tot_error(k)=mean(error);
 
-
+end
+tot_error
