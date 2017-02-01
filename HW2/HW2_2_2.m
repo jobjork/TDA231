53b890f6281edfac842b1d3cb5b713c0a04ccf8f
OlofEkborg <olofekborg@gmail.com>
2017-02-01 12:01:08 +0100

    Put the CV in a iteration loop

--- HW2/HW2_2_2.m
+++ HW2/HW2_2_2.m
@@ -2,31 +2,34 @@
 
 totalSamples = length(y);
 kFold = 5;
-sampleIndices = randperm(totalSamples);
-partitionLength = floor(totalSamples/kFold);
+tot_error=0;
+B=100; % Number of iterations
 
-samples = zeros(partitionLength, kFold);
+for b=1:B
+    sampleIndices = randperm(totalSamples);
+    partitionLength = floor(totalSamples/kFold);
 
-for k=1:kFold
-    samples(:,k) = sampleIndices(((k-1)*partitionLength +1):k*partitionLength);
-end
+    samples = zeros(partitionLength, kFold);
 
-%%
-tot_error=zeros(1,kFold);
-
-for k=1:kFold
-    testX=x(samples(:,k),:);
-    samp_train=samples;
-    samp_train(:,k)=[];
-    trainX=x(samp_train,:);
-    trainY=y(samp_train,:);
-
-    error=zeros(1,length(testX));
-    for n=1:length(testX)
-        [P1,P2,Y]=sph_bayes(testX(n,:),trainX,trainY);
-        error(n)=Y-y(samples(n,k));
+    for k=1:kFold
+        samples(:,k) = sampleIndices(((k-1)*partitionLength +1):k*partitionLength);
     end
-    tot_error(k)=mean(error);
+
+    for k=1:kFold
+        testX=x(samples(:,k),:);
+        samp_train=samples;
+        samp_train(:,k)=[];
+        trainX=x(samp_train,:);
+        trainY=y(samp_train,:);
+
+        error=zeros(1,length(testX));
+        for n=1:length(testX)
+            [P1,P2,Y]=sph_bayes(testX(n,:),trainX,trainY);
+            error(n)=Y-y(samples(n,k));
+        end
+        tot_error=tot_error+mean(error);
+
+    end
 
 end
 tot_error
