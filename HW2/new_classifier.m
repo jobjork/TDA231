1f44d1c8b3f77e683de339e6765be5cd2d691a01
Johan Björk <jobjork@hotmail.com>
2017-02-06 11:57:18 +0100

    Fixed new_classifier

--- HW2/new_classifier.m
+++ HW2/new_classifier.m
@@ -1,6 +1,7 @@
 function [Ytest] = new_classifier(Xtest, mu1, mu2)
 
 b = 0.5*(mu1 + mu2);
-Ytest = sign(((mu1 - mu2)*(Xtest - b)')/norm(mu1 - mu2, 2));
+vectorLength = length(Xtest(1,:));
+Ytest = sign(((mu1 - mu2)*(Xtest' - repmat(b, vectorLength, 1))')/norm(mu1 - mu2, 2));
 
 end
