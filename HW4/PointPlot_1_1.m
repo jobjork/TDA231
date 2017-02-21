ac763dfd9e6db3d1c81ebecf41052bcc95cb0f88
OlofEkborg <olofekborg@gmail.com>
2017-02-21 17:47:25 +0100

    modified plot in 1.1 with title etc

--- HW4/PointPlot_1_1.m
+++ HW4/PointPlot_1_1.m
@@ -1,3 +1,4 @@
+%% Problem 1.1 
 clf, close all
 x1=[2 2; 4 4; 4 0]; % Class +1
 x2=[0 0; 2 0; 0 2]; % Class -1
@@ -8,6 +9,7 @@ hold on
 scatter(x2(:,1),x2(:,2),'r','DisplayName','$y=-1$')
 plot(line,3-line,'g--','DisplayName','Hyperplane')
 axis([-1 5 -1 5])
+title('Problem 1.1','Interpreter','LaTex')
 xlabel('$x^{(1)}$','Interpreter', 'LaTex')
 ylabel('$x^{(2)}$','Interpreter', 'LaTex')
 h=legend('show');
