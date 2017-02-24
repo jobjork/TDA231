cf8e615fe5076598ae318814045876bf3ea8b3fe
OlofEkborg <olofekborg@gmail.com>
2017-02-24 16:45:04 +0100

    autoscale false som now bias and margin is correct

--- HW4/PointPlot_1_1.m
+++ HW4/PointPlot_1_1.m
@@ -1,5 +1,4 @@
 %% Problem 1.1 
-clf, close all
 x1=[2 2; 4 4; 4 0]; % Class +1
 x2=[0 0; 2 0; 0 2]; % Class -1
 line=linspace(-1,5);
@@ -10,7 +9,7 @@ scatter(x2(:,1),x2(:,2),'r','DisplayName','$y=-1$')
 plot(line,3-line,'g--','DisplayName','Hyperplane')
 axis([-1 5 -1 5])
 title('Problem 1.1','Interpreter','LaTex')
-xlabel('$x^{(1)}$','Interpreter', 'LaTex')
-ylabel('$x^{(2)}$','Interpreter', 'LaTex')
+xlabel('$x_{1}$','Interpreter', 'LaTex')
+ylabel('$x_{2}$','Interpreter', 'LaTex')
 h=legend('show');
 set(h,'Interpreter','latex')
