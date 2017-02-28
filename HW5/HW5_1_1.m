e729f5f7f802162cd95f136bce5c7a59cc142144
Johan Björk <jobjork@hotmail.com>
2017-02-28 16:56:40 +0100

    wip k means

--- HW5/HW5_1_1.m
+++ HW5/HW5_1_1.m
@@ -1,5 +1,22 @@
-%% Problem 1.1 a)
-load hw5_p1a.mat
-k=30;
-tol=1e-4;
-clusters=k_means(X,k,tol);
+%% Problem 1.1 b)
+load hw5_p1a.mat;
+k=2;
+tol=1e-5;
+
+options1 = {'numiterations', 2};
+%%
+
+clusters1=k_means(X, k, options1);
+
+%%
+clusters2=k_means(X, k);
+
+subplot(1,2,1)
+plot(X(clusters1==1, 1), X(clusters1==1, 2), 'r.', 'MarkerSize', 10)
+hold on
+plot(X(clusters1==2, 1), X(clusters1==2, 2), 'b.', 'MarkerSize', 10)
+
+subplot(1,2,2)
+plot(X(clusters2==1, 1), X(clusters2==1, 2), 'r.', 'MarkerSize', 10)
+hold on
+plot(X(clusters2==2, 1), X(clusters2==2, 2), 'b.', 'MarkerSize', 10)
