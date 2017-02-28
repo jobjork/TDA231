8c606c1cbf93ddcf4a7c8a3b36016591acaabeeb
OlofEkborg <olofekborg@gmail.com>
2017-02-28 17:33:23 +0100

    finished with 1.1 b) i think, weird results however

--- HW5/HW5_1_1.m
+++ HW5/HW5_1_1.m
@@ -1,17 +1,24 @@
 %% Problem 1.1 b)
+clear all, close all;
 load hw5_p1a.mat;
 k=2;
 tol=1e-5;
 
 clusters1=k_means(X, k, tol, false, 0);
 clusters2=k_means(X, k, tol, true, 2);
+new_assign=clusters1~=clusters2;
 
 subplot(1,2,1)
-plot(X(clusters1==1, 1), X(clusters1==1, 2), 'r.', 'MarkerSize', 10)
-hold on
-plot(X(clusters1==2, 1), X(clusters1==2, 2), 'b.', 'MarkerSize', 10)
-
-subplot(1,2,2)
 plot(X(clusters2==1, 1), X(clusters2==1, 2), 'r.', 'MarkerSize', 10)
 hold on
 plot(X(clusters2==2, 1), X(clusters2==2, 2), 'b.', 'MarkerSize', 10)
+title('Cluster assignment after 2 iterations','Interpreter','LaTex')
+
+subplot(1,2,2)
+plot(X(clusters1==1, 1), X(clusters1==1, 2), 'r.', 'MarkerSize', 10)
+hold on
+plot(X(clusters1==2, 1), X(clusters1==2, 2), 'b.', 'MarkerSize', 10)
+scatter(X(new_assign,1), X(new_assign,2), 150, 'ks');
+title('Cluster assignment at convergence ','Interpreter','LaTex')
+xlabel('$x_{1}$','Interpreter', 'LaTex')
+ylabel('$x_{2}$','Interpreter', 'LaTex')
