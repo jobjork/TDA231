3a5cf437ea36c3908faa3fd438a33700fbaa9b77
OlofEkborg <olofekborg@gmail.com>
2017-02-28 17:36:15 +0100

    modified HW5_1_1 after correct interpretation of plot

--- HW5/HW5_1_1.m
+++ HW5/HW5_1_1.m
@@ -8,13 +8,6 @@ clusters1=k_means(X, k, tol, false, 0);
 clusters2=k_means(X, k, tol, true, 2);
 new_assign=clusters1~=clusters2;
 
-subplot(1,2,1)
-plot(X(clusters2==1, 1), X(clusters2==1, 2), 'r.', 'MarkerSize', 10)
-hold on
-plot(X(clusters2==2, 1), X(clusters2==2, 2), 'b.', 'MarkerSize', 10)
-title('Cluster assignment after 2 iterations','Interpreter','LaTex')
-
-subplot(1,2,2)
 plot(X(clusters1==1, 1), X(clusters1==1, 2), 'r.', 'MarkerSize', 10)
 hold on
 plot(X(clusters1==2, 1), X(clusters1==2, 2), 'b.', 'MarkerSize', 10)
